093f30f6b4e30c880971ee57c557cde0a629abcf
Vlad Mnatsakanyan <mnatsakanyan@yual.io>
2025-03-04 17:00:39 +0200

    inheritAttrs

---
+++ components/NavItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <li class="flex-1">
+    <nuxt-link v-bind="$attrs" class="flex flex-col items-center p-2 text-xs capitalize">
+      <slot></slot>
+    </nuxt-link>
+  </li>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false
+}
+</script>
--- components/TheNav.vue
+++ components/TheNav.vue
@@ -1,11 +1,12 @@
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <li v-for="icon, page in navItems" :key="page" class="flex-1">
-        <nuxt-link :to="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize">
-          <component :is="icon" class="h-6 w-6" /> {{ page }}
-        </nuxt-link>
-      </li>
+      <NavItem 
+        v-for="icon, page in navItems" 
+        :key="page"
+        :to="`#${page}`">
+        <component :is="icon" class="h-6 w-6" /> {{ page }}
+      </NavItem>
     </ul>
   </nav>
 </template>
