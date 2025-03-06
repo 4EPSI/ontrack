55e26b5611671a9ff296b542c3812d6d15af7e5c
Vlad Mnatsakanyan <mnatsakanyan@yual.io>
2025-03-06 17:45:59 +0200

    selectedActivityId

--- components/TimelineItem.vue
+++ components/TimelineItem.vue
@@ -2,28 +2,16 @@
   <li 
     class="relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4" 
   >
-    <nuxt-link 
-      :class="hourLinkClasses" 
-      to="/">
-        {{ timelineItem.hour }}:00
+    <nuxt-link :class="hourLinkClasses" to="/">
+      {{ timelineItem.hour }}:00
     </nuxt-link>
 
-    <div class="flex gap-2">
-      <button class="rounded bg-gray-100 p-3 enabled:hover:bg-gray-200 disabled:cursor-not-allowed disabled:opacity-50">
-        <XMarkIcon class="h-8" />
-      </button>
-      <select name="" id="" class="w-full truncate rounded bg-gray-100 py-1 px-2 text-2xl">
-        <option selected disabled value="">Rest</option>
-        <option v-for="{ value, label } in options" :key="value" :value="value">
-          {{ label }}
-        </option>
-      </select>
-    </div>
+    <BaseSelect :selected="selectedActivityId" :options="options" placeholder="Rest" />
   </li>
 </template>
 
 <script setup>
-import { XMarkIcon } from '@heroicons/vue/24/outline';
+import BaseSelect from './BaseSelect.vue'
 
 const props = defineProps(['timelineItem'])
 
@@ -39,4 +27,6 @@ const options = [
   { value: '2', label: 'Reading' },
   { value: '3', label: 'Training' },
 ]
+
+const selectedActivityId = ref('3')
 </script>
