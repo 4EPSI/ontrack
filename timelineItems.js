3cbd8fd72242de3bac06c09bef8ca5e1e18612ab
Vlad Mnatsakanyan <mnatsakanyan@yual.io>
2025-03-25 19:30:50 +0400

    generateTimelineItems

--- timelineItems.js
+++ timelineItems.js
@@ -1,10 +1,10 @@
-import { generateTimelineItems } from "./functions";
 import { useActivities } from "./activities";
+import { HOURS_IN_DAY } from "./constants";
 
 // export const timelineItems = useState('timelineItems', () => generateTimelineItems(activities.value));
 export const useTimelineItems = () => {
   const activities = useActivities();
-  return useState('timelineItems', () => generateTimelineItems(activities.value));
+  return useState('timelineItems', () => generateTimelineItems(activities));
 }
 
 export const setTimelineItemActivity = (timelineItem, activityId) => {
@@ -13,4 +13,14 @@ export const setTimelineItemActivity = (timelineItem, activityId) => {
 
 export const updateTimelineItemActivitySeconds = (timelineItem, activitySeconds) => {
   timelineItem.activitySeconds += activitySeconds
+}
+
+const generateTimelineItems = (activities) => {
+  return [...Array(HOURS_IN_DAY).keys()].map((hour) => ({
+    hour,
+    activityId: [0,1,2,3,4].includes(hour) ? activities.value[hour % 3].id : null,
+    activitySeconds: [0,1,2,3,4].includes(hour) ?hour * 600 : 0
+    // activityId: hour % 4 === 0 ? null : activities[hour % 2].id,
+    // activitySeconds: hour % 4 === 0 ? 0 :(15 * SECONDS_IN_MINUTE * hour) % SECONDS_IN_HOUR
+  }))
 }
